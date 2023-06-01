b1dd12b0140ac351b41b26ade7d0a783cfe206dd
Hynek <hyneksvacha@seznam.cz>
2023-06-01 11:19:23 +0200

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-
-use Hyneks\AdministrativeUnitsCzechRepublic;
-
-$faker = \Faker\Factory::create();
-$faker->addProvider(new AdministrativeUnitsCzechRepublic($faker));
-
-print_r($faker->hierarchy());
